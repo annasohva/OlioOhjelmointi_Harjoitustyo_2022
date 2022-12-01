9d6934a288e68c56b68fbcb8cc726403bb3a5d2f
annasohva <100351136+annasohva@users.noreply.github.com>
2022-12-01 12:39:21 +0200

    Projekti luotu

---
+++ Laskutussovellus_Harjoitustyo/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace Laskutussovellus_Harjoitustyo {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
